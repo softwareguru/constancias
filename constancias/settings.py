8567f2db6898b327cd29ff42a55665a6c3e5fdaa
Pedro Galvan <pedro@sg.com.mx>
2021-11-02 05:48:47 -0500

    Static config

--- constancias/settings.py
+++ constancias/settings.py
@@ -114,6 +114,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
