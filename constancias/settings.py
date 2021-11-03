f0db3bfaf8951b06edbb54fdf72a4688e1cc6e39
Constancias <constancias@vps3.sg.com.mx>
2021-11-03 01:51:28 -0600

    Updated settings

--- constancias/settings.py
+++ constancias/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST')]
 
 
 # Application definition
